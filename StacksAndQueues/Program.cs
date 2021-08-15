09756a585ea33c358d42c745a9d177cbe5a3bbfc
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-15 15:25:39 -0600

    feat: Created stack list

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -7,6 +7,12 @@ namespace StacksAndQueues
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to stack and queues progrmas");
+            Stacklist list = new Stacklist();
+            list.push(70);
+            list.push(30);
+            list.push(56);
+            list.Display();
+
         }
     }
 }
---
+++ StacksAndQueues/Stacklist.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues
+{
+    class Stacklist
+    {
+        private Node top;
+        public Stacklist()
+        {
+            this.top = null;
+        }
+        internal void push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pushed to stack ", value);
+        }
+         internal void Display()
+        {
+            if (this.top == null)
+                Console.WriteLine("list is empty.");
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.WriteLine("Data : " + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
